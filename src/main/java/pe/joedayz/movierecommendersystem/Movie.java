e16edda85134e6d9b045f46f16cd14363d9316be
Jose Diaz <jose@exoreaction.com>
2024-11-07 19:07:55 -0500

    movie

--- src/main/java/pe/joedayz/movierecommendersystem/Movie.java
+++ src/main/java/pe/joedayz/movierecommendersystem/Movie.java
@@ -7,4 +7,19 @@ import org.springframework.stereotype.Component;
 @Scope("prototype")
 public class Movie {
 
+  private static int instances = 0;
+
+  private int id;
+  private String name;
+  private String genre;
+  private String producer;
+
+  public Movie() {
+    instances++;
+    System.out.println("Movie constructor called");
+  }
+
+  public static int getInstances() {
+    return instances;
+  }
 }

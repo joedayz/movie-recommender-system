ab4fc52e0df5441d09b88cb66ce6268141682f85
Jose Diaz <jose@exoreaction.com>
2024-11-07 19:18:53 -0500

    usando proxy

--- src/main/java/pe/joedayz/movierecommendersystem/Movie.java
+++ src/main/java/pe/joedayz/movierecommendersystem/Movie.java
@@ -1,10 +1,12 @@
 package pe.joedayz.movierecommendersystem;
 
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
 
 @Component
-@Scope("prototype")
+@Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE, proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class Movie {
 
   private static int instances = 0;
